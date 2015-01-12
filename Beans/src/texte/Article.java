4039d4e862e407a6e9dea5b7553fdc8a5cd79049
Lolsety <damien2lage@gmail.com>
2015-01-12 09:26:03 +0100

    Contenu changé

--- Beans/src/texte/Article.java
+++ Beans/src/texte/Article.java
@@ -4,33 +4,18 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 
-//@Stateful
+
 public class Article extends Texte {
+	private final String URL = "";
 	private int id;
 	String titre;
 	
-	public Article(compte.Compte compte, String text, int datePub, String titre,int id){
+	public Article(compte.Compte compte, int datePub, String titre,int id){
 		this.id =id;
 		this.auteur = compte;
-		this.text = "C:\\Users\\Damien\\Desktop\\" +Integer.toString(this.id)+ ".txt";
+		this.text = URL + Integer.toString(this.id)+ ".txt";
 		this.datePub = datePub;
 		this.titre = titre;
-		final File fichier =new File(this.text);
-		try {
-		       // Creation du fichier
-		       fichier .createNewFile();
-		       // creation d'un writer (un écrivain)
-		       final FileWriter writer = new FileWriter(fichier);
-		       try {
-		           writer.write(text);
-		       } finally {
-		           // quoiqu'il arrive, on ferme le fichier
-		           writer.close();
-		       }
-		   } catch (Exception e) {
-			   e.printStackTrace();
-		       System.out.println("Impossible de creer le fichier");
-		   }
 	}
 
 	public String getTitre() {
@@ -41,8 +26,7 @@ public class Article extends Texte {
 		this.titre = titre;
 	}
 	
-	@Override
-	public String getText() {
+	public String getContenu() {
 		 String article="";
 		 int c;
 		 try {
@@ -56,8 +40,24 @@ public class Article extends Texte {
 				 reader.close();
 			 }
 		 } catch (Exception e) {
-	            System.out.println("Impossible de lire fichier");
+	            System.out.println("Impossible de lire le fichier");
 	      }
 		return article;
+	}
+	
+	public void setContenu(String text) {		
+		try {
+			final FileWriter writer = new FileWriter(this.text);
+			try {
+				writer.write(text);
+			} finally {
+	           // quoiqu'il arrive, on ferme le fichier
+	           writer.close();
+			}
+	    } catch (Exception e) {
+		   e.printStackTrace();
+	       System.out.println("Impossible de modifier le fichier");
+	   }
+		
 	}
 }
